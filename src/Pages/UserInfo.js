9dd32f330b4b7b506b235a53065426161af6516a
rokib97 <hasanrokib1997@gmail.com>
2022-06-10 22:17:41 +0600

    netlify 404 solve

--- src/Pages/UserInfo.js
+++ src/Pages/UserInfo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const UserInfo = () => {
@@ -9,45 +9,25 @@ const UserInfo = () => {
     fetch(`https://randomuser.me/api/?uuid=${userId}`)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         setUser(data.results[0]);
       });
   }, [userId]);
-  console.log(user?.dob?.date);
 
-  //   month day year
-  const birthday = new Date("01/01/2023");
+  // tried to implement but could not finish it
+  const birthday = new Date(user?.dob?.date);
 
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-  let timerId;
 
-  function countDown() {
-    const today = new Date();
-    const timeSpan = birthday - today;
-    //milliseconds
-    console.log(timeSpan);
+  const today = new Date();
+  const timeSpan = birthday - today;
+  //milliseconds
 
-    if (timeSpan <= -day) {
-      console.log("Hope you had a nice Birthday!!");
-      clearInterval(timerId);
-      return;
-    }
+  const days = Math.floor(timeSpan / day);
 
-    if (timeSpan <= 0) {
-      console.log("Happy Birthday!!");
-      clearInterval(timerId);
-      return;
-    }
-
-    const days = Math.floor(timeSpan / day);
-    const hours = Math.floor((timeSpan % day) / hour);
-    const minutes = Math.floor((timeSpan % hour) / minute);
-    const seconds = Math.floor((timeSpan % minute) / second);
-  }
-
-  timerId = setInterval(countDown, second);
   return (
     <div class="card lg:w-3/6 mx-auto bg-base-100 shadow-xl px-12 mt-12">
       <div class="avatar">
@@ -57,7 +37,6 @@ const UserInfo = () => {
       </div>
       <div class="card-body">
         <h2 class="card-title">Phone Number: {user?.phone}</h2>
-        <p>Birthday Countdown: </p>
       </div>
     </div>
   );
